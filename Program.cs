d092abd0c9ccceea6ae51fd4675e3ad9b6c548d2
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-03-10 20:11:50 -0300

    Feat: ADO.NET

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
-﻿const string connectionString = "Server=localhost,1433;Database=balta_cursoAcessoADados;User ID=sa;Password=MyPassword123#";
+﻿using Microsoft.Data.SqlClient;
 
+const string connectionString = "Server=localhost,1433;Database=balta_cursoAcessoADados;User ID=sa;Password=MyPassword123#";
 
-Console.WriteLine("Hello, World!");
+using (var connection = new SqlConnection(connectionString))
+{
+  System.Console.WriteLine("Conectado!");
+  connection.Open();
+
+  using (var command = new SqlCommand())
+  {
+    command.Connection = connection;
+    command.CommandType = System.Data.CommandType.Text;
+    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+    var reader = command.ExecuteReader();
+
+    while (reader.Read())
+    {
+      System.Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+    }
+  }
+  connection.Close();
+
+}
+
--- obj/Debug/net7.0/AcessoADados-SQLServer-Dapper.AssemblyInfo.cs
+++ obj/Debug/net7.0/AcessoADados-SQLServer-Dapper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcessoADados-SQLServer-Dapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b22939104022bf1c12e9f406d7c726bb7b6f625")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcessoADados-SQLServer-Dapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcessoADados-SQLServer-Dapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
